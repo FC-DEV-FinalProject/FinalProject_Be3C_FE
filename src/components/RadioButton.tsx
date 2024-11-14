aac7fa9080056bb03fe3b32fe23c5463ce10011a
LeeYunHwan <cyzhqly@gmail.com>
2024-11-14 17:32:18 +0900

    style: 라디오 버튼에 hover 속성 추가 & 속성 명 및 구조 변경 #26

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -3,7 +3,7 @@ import { COLOR, COLOR_OPACITY } from '@/constants/color';
 import { FONT_SIZE, FONT_WEIGHT } from '@/constants/font';
 
 interface RadioOptionProps {
-  text: string;
+  label: string;
   value: string;
 }
 
@@ -23,13 +23,15 @@ const RadioButton: React.FC<RadioBtnProps> = ({
   <>
     {options.map((option) => (
       <label key={option.value} css={labelWrapperStyle}>
-        <input
-          type='radio'
-          name={name}
-          checked={selected === option.value}
-          onChange={() => onChange(option.value)}
-        />
-        {option.text}
+        <div>
+          <input
+            type='radio'
+            name={name}
+            checked={selected === option.value}
+            onChange={() => onChange(option.value)}
+          />
+        </div>
+        {option.label}
       </label>
     ))}
   </>
@@ -43,10 +45,19 @@ const labelWrapperStyle = css`
   font-weight: ${FONT_WEIGHT.REGULAR};
   letter-spacing: -0.32px;
 
+  div {
+    border-radius: 50%;
+
+    &:hover {
+      background-color: ${COLOR_OPACITY.PRIMARY_OPACITY10};
+      transition: 0.3s;
+    }
+  }
+
   input[type='radio'] {
-    width: 24px;
-    height: 24px;
-    margin: 12px;
+    width: 20px;
+    height: 20px;
+    margin: 10px;
     accent-color: ${COLOR.PRIMARY};
   }
 `;
